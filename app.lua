f6fd43c0796402fbbd6ac31eb6f97be01bfe2606
csaba.sallai <csaba.sallai@innoveo.com>
2024-06-24 14:12:56 +0200

    NOTICKET Creating Views, passing dynamic values, ...etc

--- app.lua
+++ app.lua
@@ -1,8 +1,25 @@
 local lapis = require("lapis")
 local app = lapis.Application()
+app:enable("etlua")
+app.layout = require "views.layout"
 
-app:get("/", function()
-  return "Welcome to Lapis " .. require("lapis.version")
+app:get("/", function(self)
+  self.page_title = "Hello, World!"
+  self.welcome_message = "Welcome to my dynamic Lapis page!"
+  print("Page Title:", self.page_title)       -- Debugging statement
+  print("Welcome Message:", self.welcome_message) -- Debugging statement
+  return { render = "index" }
+end)
+
+app:get("/list", function(self)
+  return { render = "list" }
+end)
+
+app:get("/basic", function(self)
+  return { render = "basic" }
 end)
 
 return app
+
+
+
